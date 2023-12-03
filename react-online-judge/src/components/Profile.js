d3695be30fae62e73d75a140888c54cf9079f3a6
RIHU <rihu0311@gmail.com>
2023-12-04 07:32:42 +0900

    Update Profile.js

--- react-online-judge/src/components/Profile.js
+++ react-online-judge/src/components/Profile.js
@@ -1,14 +1,15 @@
-import { useParams } from "react-router-dom";
 import userData from "../data/userData";
-import { hover } from "@testing-library/user-event/dist/hover";
-const Profile = () => {
-	const { userId } = useParams();  // react-router-dom 에 정의된 후크
-	const user = userData[userId];
+import { useSelector } from "react-redux";
+import { selectLogin } from "./LoginReducer";
 
-	if (!user) {
+const Profile = () => {
+	const loginState = useSelector(selectLogin);
+	const userID = loginState.userID;
+
+	if (!userID) {
 		return <span>유저를 찾을 수 없습니다.</span>
 	}
-	const { nickname, password, email, exp, solved } = user;
+	const { nickname, password, email, exp, solved } = userData[userID];
 	const solvedPoblems = [];
 	function solvedCount(solved) {
 		let count = 0;
@@ -25,12 +26,12 @@ const Profile = () => {
 			<img className="profileImg" style={{height:"300px", borderRadius:"50%", 
 			border:"2px solid #111", marginBottom:"10px"}
 		} 
-			src={`/img/${userId}.png`} alt="profileImage"/>
+			src={`/img/${userID}.png`} alt="profileImage"/>
 			<br/>
 			<h1>{nickname}</h1>
-			<img src={`http://mazassumnida.wtf/api/v2/generate_badge?boj=${userId}`}
+			<img src={`http://mazassumnida.wtf/api/v2/generate_badge?boj=${userID}`}
 			 alt="solveacImg"></img>
-			<p>{userId}</p>
+			<p>{userID}</p>
 			<p>{email}</p>
 			<p>경험치 : {exp}</p>
 			<p>푼 문제 : 총 {solvedCount(solved)}개</p>
