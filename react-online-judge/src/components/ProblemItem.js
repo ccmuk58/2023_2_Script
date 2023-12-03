6aa934d560c6887d6a09e517ffc2ce5263a88e6a
Nakuri <ccmuk58@naver.com>
2023-12-04 06:43:19 +0900

    Feat: Profile Image

--- react-online-judge/src/components/ProblemItem.js
+++ react-online-judge/src/components/ProblemItem.js
@@ -1,34 +1,37 @@
 import { useParams } from "react-router-dom";
+import Form from 'react-bootstrap/Form';
 import problemData from "../data/problemData";
-
 const ProblemItem = () => {
 	const { problemId } = useParams();  // react-router-dom 에 정의된 후크
 	const problem = problemData[problemId];
 
 	if (!problem) {
-		return <span>문제를 찾을 수 없습니다.</span>
+		return <span>undefined</span>
 	}
-	const { title, description, difficulty, input, output } = problem;
+	const { title, description, difficult, input, output } = problem;
 	return (
 		<div className="content-item">
 			<h3>{title}</h3>
 			<hr />
-			난이도: {difficulty} | 시간 제한 : 1초 | 메모리 제한 : 1MB | 푼 사람: 0명
-			<hr />
 			<p>{description}</p>
 			<hr />
 
-			<div className="examples">
-				<div className="example">
-					<p>입력</p>
-					<p className="IO">{input}</p>
-				</div>
-				<div className="example">
-					<p>출력</p>
-					<p className="IO">{output}</p>
-				</div>
+			<div className="example">
+				<Form.Label>입력</Form.Label>
+				<Form.Control
+					value={input}
+					disabled
+					className="IO"
+				/>
 			</div>
-
+			<div className="example">
+			<Form.Label>출력</Form.Label>
+				<Form.Control
+					value={output}
+					disabled
+					className="IO"
+				/>
+			</div>
 		</div>
 	);
 }
--- react-online-judge/src/components/Profile.js
+++ react-online-judge/src/components/Profile.js
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import userData from "../data/userData";
-
 const Profile = () => {
 	const { userId } = useParams();  // react-router-dom 에 정의된 후크
 	const user = userData[userId];
@@ -22,7 +21,9 @@ const Profile = () => {
 	}
 	return (
 		<div className="content-item">
-
+			<img style={{height:"300px", borderRadius:"50%", 
+			border:"2px solid #111", marginBottom:"10px"}} 
+			src={`/img/${userId}.png`} alt="profileImage"/>
 			<h1>{nickname}</h1>
 			<p>{userId}</p>
 			<p>{email}</p>
