763c1db006064a52e76d91d95a092a6e5da1eea1
Nakuri <ccmuk58@naver.com>
2023-12-04 05:34:58 +0900

    Create userData.js

---
+++ react-online-judge/src/data/userData.js
@@ -0,0 +1,32 @@
+const userData = {
+    'ccmuk58': {
+        userid: 'ccmuk58',
+		nickname: '나쿠리',
+        password: '1234',
+        email: 'ccmuk58@naver.com',
+		exp : 0,
+		solved : {
+			'1' : true,
+			'2' : false,
+			'3' : false,
+			'4' : true,
+			'5' : false,
+		},
+	},
+
+	'rihu':{
+		userid: 'rihu',
+		nickname: '핵가야',
+        password: '5678',
+        email: 'rihu@naver.com',
+		exp : 0,
+		solved : {
+			'1' : false,
+			'2' : true,
+			'3' : false,
+			'4' : true,
+			'5' : false,
+		},
+	}
+};
+export default userData;
