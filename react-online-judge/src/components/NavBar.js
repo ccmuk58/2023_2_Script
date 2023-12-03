84a1282aeddb65ab5e58481e1e6ad9932b2b8a1b
Nakuri <ccmuk58@naver.com>
2023-12-03 23:44:06 +0900

    Fix: type

--- react-online-judge/src/components/NavBar.js
+++ react-online-judge/src/components/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = () => {
         <NavLink to='/ranking' style={{padding: 5}}>랭킹</NavLink>
         <NavLink to='/search' style={{padding: 5}}>검색</NavLink>
         <NavLink to='/profile' style={{padding: 5}}>프로필</NavLink>
-        <NavLink to='/Login' style={{padding: 5}}>로그인</NavLink>
+        <NavLink to='/login' style={{padding: 5}}>로그인</NavLink>
       </nav>
     </div>
   );
