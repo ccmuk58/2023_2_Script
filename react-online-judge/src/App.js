7f12cf821ca2991b5608d62eb38c62ff65b2a3e1
Nakuri <ccmuk58@naver.com>
2023-12-03 20:00:38 +0900

    Update App.js

--- react-online-judge/src/App.js
+++ react-online-judge/src/App.js
@@ -1,5 +1,3 @@
-  import { BrowserRouter as Router, Routes, Route, Link, NavLink, 
-    Outlet, useParams, useNavigate, Navigate } from 'react-router-dom';
   import './App.css';
   import { useState } from 'react';
   import Profile from './components/Pages/Profile';
@@ -7,66 +5,16 @@
   import ProtectRoute from './components/ProtectRoute';
   import {Login, Logout} from './components/Login';
 
-  const About = () => {
-    return (
-      <h1>s</h1>
-    );
-  }
 
-  const NoPage = () => {
-    return (
-      <div style={{padding: 20}}>
-        <h1>404: Page Not Found</h1>
-        <p>The page is not exist.</p>
-      </div>
-    );
-  }
 
-  const AdminPage = () => {
-    return (
-      <div style={{padding: 20}}>
-        <h1>ADMIN page</h1>
-        <p>This is a Admin page view.</p>
-      </div>
-    );
-  }
-
-  const AppRouter = () => {
-    const [user, setUser] = useState();
-    const navigate = useNavigate();
-    return (
-      <>
-        <nav style={{margin: 10}}>
-          <NavLink to='/' style={{padding: 5}}>Home</NavLink>
-          <NavLink to='/about' style={{padding: 5}}>About</NavLink>
-          {user && <NavLink to='/admin' style={{padding: 5}}>Admin</NavLink>}
-          <span> | </span>
-          {!user && <NavLink to='/login' style={{padding: 5}}>Login</NavLink>}
-          {user && <NavLink to='/profile' style={{padding: 5}}>Profile</NavLink>}
-          {user && <NavLink to='/logout' style={{padding: 5}}>Logout</NavLink>}
-        </nav>
-
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/admin' 
-            element={<ProtectRoute user={user}><AdminPage/></ProtectRoute>}/>
-          <Route path='*' element={<NoPage/>}/>
-          <Route path='/login' element={<Login setUser={setUser} />}></Route>
-          <Route path='/profile' element={<Profile user = {user} />}></Route>
-          <Route path='/logout' 
-                  element={<Logout setUser={setUser} navigate={navigate}/>}></Route>
-        </Routes>    
-      </>
-    );
-  }
-
-  function App() {
-    return (
-      <Router>
-        <AppRouter />
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        \
+      </header>
+    </div>
+  );
+}
 
   export default App;
