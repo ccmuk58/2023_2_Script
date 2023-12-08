342c0fcfda569c3f117dd22084e828eafade1744
Nakuri <ccmuk58@naver.com>
2023-12-09 03:14:13 +0900

    Feat: Problem Input Output Temp

--- react-online-judge/src/components/ProblemItem.js
+++ react-online-judge/src/components/ProblemItem.js
@@ -1,34 +1,111 @@
 import { useParams } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
 import problemData from "../data/problemData";
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
+import JDoodleAPI from "../JDoodleAPI";
+
 const ProblemItem = () => {
 	const { problemId } = useParams();  // react-router-dom 에 정의된 후크
 	const problem = problemData[problemId];
 
+	const [code, setCode] = useState('');
+	const [language, setLanguage] = useState('cpp17');
+	const [output, setOutput] = useState('');
+	const [cpuTime, setCpuTime] = useState('');
+	const [memory, setMemory] = useState('');
+	const [loading, setLoading] = useState(false);
+
+	const handleCodeChange = (e) => {
+		setCode(e.target.value);
+	};
+	
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+		const result = await JDoodleAPI.executeCode(code, language);
+		setLoading(false);
+		console.log(result);
+		setOutput(result.output);
+		setCpuTime(result.cpuTime + ' sec');
+		setMemory(result.memory + ' bytes');
+	};
+
+
 	if (!problem) {
 		return <span>undefined</span>
 	}
-	const { title, description, difficulty, input, output } = problem;
+	const { title, description, difficulty, exinput, exoutput } = problem;
 	return (
-		
+
 		<div className="content-item">
-			<h3>{title}</h3>
+			<h2 className="problemHeader">{title}</h2>
 			<hr />
 			난이도: {difficulty} | 시간 제한 : 1초 | 메모리 제한 : 1MB | 푼 사람: 0명
 			<hr />
 			<p>{description}</p>
 			<hr />
 
-			<div className="examples">
+			<div className="content-item-div">
 				<div className="example">
 					<p>입력</p>
-					<p className="IO">{input}</p>
+					<p className="IO">{exinput}</p>
 				</div>
 				<div className="example">
 					<p>출력</p>
-					<p className="IO">{output}</p>
+					<p className="IO">{exoutput}</p>
 				</div>
 			</div>
+			<Form onSubmit={handleSubmit}>
+				<Form.Group controlId="language">
+					<Form.Label>언어</Form.Label>
+					<Form.Control as="select" value={language} onChange={(e) => setLanguage(e.target.value)}>
+						<option value="cpp17">C++17</option>
+						<option value="c">C</option>
+						<option value="python3">Python3</option>
+						<option value="java">Java</option>
+						<option value="php">PHP</option>
+						<option value="csharp">C#</option>
+						<option value="pascal">Pascal</option>
+						<option value="objc">Objective-C</option>
+						<option value="rust">Rust</option>
+						<option value="ruby">Ruby</option>
+						<option value="perl">Perl</option>
+						<option value="scala">Scala</option>
+						<option value="swift">Swift</option>
+						<option value="go">Go</option>
+						<potion value="nodejs">NodeJS</potion>
+					</Form.Control>
+				</Form.Group>
+				<Form.Group controlId="code">
+					<Form.Label className="mt-3">소스 코드</Form.Label>
+					<Form.Control as="textarea" rows={10} value={code} onChange={handleCodeChange} />
+				</Form.Group>
+				<Button variant="primary" type="submit" disabled={loading} className="mt-1">
+					{loading ? '채점중..' : '제출'}
+				</Button>
+
+				{
+					// 3개의 폼 한줄에 나오게
+				}
+				<div className="content-item-div">
+					<Form.Group controlId="cpuTime" className="m-3 ">
+						<Form.Label>실행 시간</Form.Label>
+						<Form.Control type="text" value={cpuTime} readOnly />
+					</Form.Group>
+					<Form.Group controlId="memory" className="m-3">
+						<Form.Label>메모리</Form.Label>
+						<Form.Control type="text" value={memory} readOnly />
+					</Form.Group>
+					<Form.Group controlId="output" className="m-3">
+						<Form.Label>결과</Form.Label>
+						<Form.Control type="text" value={output} readOnly />
+					</Form.Group>
+				</div>
+				
+
+			</Form>
+
 
 		</div>
 	);
--- react-online-judge/src/data/problemData.js
+++ react-online-judge/src/data/problemData.js
@@ -1,16 +1,25 @@
 // problemData.js
 const problemData = {
 	'1': {
-		title: '장현이가 대충 만든 문제',
+		title: '형진이의 정렬',
 		description: `
-		장현이는 기말 프로젝트의 추가하기위해 문제를 대충 만든다.\n
-		앗! 문제를 대충 만들다가 문제를 잘못 만들었다. 이 문제를 해결해주자.\n
-		입력은 첫 줄에는 정수의 개수 N이 주어지고, 두 번째 줄에는 N개의 정수가 주어진다.\n
-		출력에는 M개의 정수를 출력한다.\n
+		요즘 형진이는 장현이에게 알고리즘 과외를 받고있다.
+		오늘은 정렬을 배우는 날이다.
+		장현이는 형진이에게 다음과 같은 문제를 주었다.
+		
+		"주어진 수열을 내림차순으로 정렬하시오."
+		
+		형진이는 이 문제를 풀 수 있을까?
+		형진이를 도와 문제를 풀어주자.
+		
 		`,
 		difficulty: 'Silver',
-		input: `5\n1 3 4 2 5`,
-		output: '5 4 3 2 1',
+		inputLimit: '첫 번째 줄에 수열의 크기 N이 주어진다. (1 ≤ N ≤ 10)\n두 번째 줄에 N개의 정수가 주어진다. 이 수는 모두 1 이상 100 이하이다.',
+		ouputLimit: '주어진 수열을 내림차순으로 정렬한 결과를 출력한다.',
+		exinput: `5\n1 3 4 2 5`,
+		exoutput: '5 4 3 2 1',
+		input:["5 1 3 4 2 5", "10 1 2 3 4 5 6 7 8 9 10", "1 10"],
+		output:["5 4 3 2 1", "10 9 8 7 6 5 4 3 2 1", "10"],
 		solved: false,
 		algorithms: ['정렬'],
 	},
@@ -39,8 +48,8 @@ const problemData = {
 		------------------------
 		`,
 		difficulty: 'Bronze',
-		input: 'Snow',
-		output: 'Wrong Answer',
+		exinput: 'Snow',
+		exoutput: 'Wrong Answer',
 		solved: false,
 		algorithms: ['애드혹'],
 	},
@@ -48,8 +57,8 @@ const problemData = {
 		title: '제목 3',
 		description: 'This is the third problem.',
 		difficulty: 'Gold',
-		input: '5\n1 2 3 4 5',
-		output: '3 7',
+		exinput: '5\n1 2 3 4 5',
+		exoutput: '3 7',
 		solved: false,
 		algorithms: ['수학'],
 	},
