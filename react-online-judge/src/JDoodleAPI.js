5dc4eb38dc23ccc55f60a8954df2784a8124f050
Nakuri <ccmuk58@naver.com>
2023-12-09 02:57:08 +0900

    Add: JDoodleAPI

---
+++ react-online-judge/src/JDoodleAPI.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const JDoodleAPI = {
+	executeCode: async (code, language) => {
+		const url = '/jdoodle';
+		const program =
+		{
+			hasInputFiles:false,
+			language:language,
+			libs:[],
+			projectKey:1001,
+			script:code,	
+			stdin:"",
+			versionIndex:0,
+			clientId: process.env.REACT_APP_JDOODLE_CLIENT_ID,
+			clientSecret: process.env.REACT_APP_JDOODLE_CLIENT_SECRET,
+		};
+		try {
+			const response = await axios.post(url, program);
+			return response.data;
+		} catch (error) {
+			console.error('Error executing code:', error);
+			throw error;
+		}	
+	},
+};
+export default JDoodleAPI;
