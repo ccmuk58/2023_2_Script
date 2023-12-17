fb386ad5d4cebc40a1a9a8049f13b00818d10d43
Nakuri <ccmuk58@naver.com>
2023-12-17 19:24:10 +0900

    Feat: Temp Profile Design
    클래스따라 CSS 바뀌게 해야 함 !

--- react-online-judge/src/components/Profile.js
+++ react-online-judge/src/components/Profile.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { db } from "./firebaseinit";
-import { collection, getDoc,doc, query, where } from 'firebase/firestore';
+import { collection, getDoc, doc, query, where } from 'firebase/firestore';
 import { useState } from "react";
 import { useEffect } from "react";
 
@@ -10,23 +10,26 @@ const Profile = () => {
 	const fetchData = async () => {
 		const docRef = doc(db, "Members", userId);
 		const docSnap = await getDoc(docRef);
-  
+
 		if (docSnap.exists()) {
-		  console.log("Document data:", docSnap.data());
-		  setUser(docSnap.data());
+			console.log("Document data:", docSnap.data());
+			setUser(docSnap.data());
 		} else {
-		  console.log("No such document!");
+			console.log("No such document!");
 		}
-	  };
+	};
 	useEffect(() => {
-	  fetchData(); // 컴포넌트가 마운트될 때 데이터를 가져오기 위해 함수 호출
+		fetchData(); // 컴포넌트가 마운트될 때 데이터를 가져오기 위해 함수 호출
 	}, []);
-  
+
 	if (!user) {
-	  return <span>유저를 찾을 수 없습니다.</span>
+		return <span>유저를 찾을 수 없습니다.</span>
 	}
 	const { nickname, ID, PW, email, exp, solved } = user;
+	const classes = ["Bronze", "Silver", "Gold", "Platinum", "Diamond", "Ruby"];
 	const solvedProblems = [];
+	const userClass = classes[Math.floor(exp / 100)];
+	const userClassExp = 100 - exp % 100;
 	function solvedCount(solved) {
 		let count = 0;
 		for (let i = 0; i < solved.length; i++) {
@@ -48,19 +51,35 @@ const Profile = () => {
 				}
 					src={`/img/${ID}.png`} alt="profileImage" />
 				<br />
-				<h1>{nickname}</h1>
-				<a href={`https://solved.ac/${ID}`}>
-					<img src={`http://mazassumnida.wtf/api/v2/generate_badge?boj=${ID}`}
-						alt="solveacImg"></img>
-				</a>
-				<p>{ID}</p>
+				<h1 style={{ fontWeight: "bold" }}>{nickname}</h1>
 				<p>{email}</p>
-				<p>경험치 : {exp}</p>
-				<p>푼 문제 : 총 {solvedCount(solved)}개</p>
-				<p>푼 문제 번호</p>
-				{solvedProblems.map((problemId) => (
-					<p style={{ display: "inline-block" }}>{problemId}&nbsp;</p>
-				))}
+
+				<div className="content-item-div">
+					<p>클래스 : {userClass}</p>
+					<p>총 경험치 : {exp}</p>
+					<p>남은 경험치 : {userClassExp}</p>
+					<div className="exp-bar">
+						<div className="exp-bar-inner" style={{ width: `${userClassExp}%` }}></div>
+					</div>
+				</div>
+				<div className="content-item-div">
+					<p>푼 문제 : 총 {solvedCount(solved)}개</p>
+					{solvedProblems.map((problemId) => (
+						<p style={{ display: "inline-block" }}>{problemId}&nbsp;</p>
+					))}
+				</div>
+				<div className="content-item-div">
+
+					<div>
+						<p>Solve.ac 프로필</p>
+						<a href={`https://solved.ac/${ID}`}>
+							<img src={`http://mazassumnida.wtf/api/v2/generate_badge?boj=${ID}`}
+								alt="solveacImg"></img>
+						</a>
+
+					</div>
+
+				</div>
 			</div>
 		</>
 	);
