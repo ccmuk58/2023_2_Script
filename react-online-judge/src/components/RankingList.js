70b564c0bc7f73f7755d3795c3facde8a7fd5208
RIHU <rihu0311@gmail.com>
2023-12-17 16:34:35 +0900

    문제 검색 DB 활용

--- react-online-judge/src/components/RankingList.js
+++ react-online-judge/src/components/RankingList.js
@@ -1,41 +1,38 @@
+//RankingList.
 import { Link } from "react-router-dom";
 import { Table } from "react-bootstrap";
 import { db } from "./firebaseinit";
-import { collection, query, orderBy, getDocs } from 'firebase/firestore';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
 import { useEffect, useState } from "react";
 
 const RankingList = () => {
   const [sortedMembers, setSortedMembers] = useState([]);
 
-  const getMembers = async () => {
-    const q = query(collection(db, 'Members'), orderBy('exp', 'desc'));
-    const querySnapshot = await getDocs(q);
-    const members = [];
-    querySnapshot.forEach((doc) => {
-      // 문서의 데이터와 ID를 가져와서 객체로 만듭니다.
-      members.push({ userKey: doc.id, ...doc.data() });
-    });
-    return members;
-	
-  };
-
   useEffect(() => {
-    const fetchMembers = async () => {
-      const members = await getMembers();
+    // 쿼리 정의
+    const q = query(collection(db, 'Members'), orderBy('exp', 'desc'));
+
+    // onSnapshot을 사용하여 실시간 업데이트를 감지하고 초기 데이터를 가져옴
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const members = snapshot.docs.map((doc) => ({
+        userKey: doc.id,
+        ...doc.data(),
+      }));
       setSortedMembers(members);
-    };
+    });
 
-    fetchMembers();
-  }, []);
+    // 컴포넌트가 언마운트될 때 구독을 해제
+    return () => unsubscribe();
+  }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때만 실행되도록 함 
 
-  function getClass(exp) {
+  const getClass = (exp) => {
     if (exp < 100) return "Bronze";
     else if (exp < 200) return "Silver";
     else if (exp < 300) return "Gold";
     else if (exp < 400) return "Platinum";
     else if (exp < 500) return "Diamond";
     else return "Master";
-  }
+  };
 
   return (
     <div className="content-item">
@@ -50,8 +47,8 @@ const RankingList = () => {
           </tr>
         </thead>
         <tbody>
-          {sortedMembers.map(({ userKey, ID, nickname, exp, ranking }, index) => (
-            <tr key={userKey} to={`/profile/${userKey}`}>
+          {sortedMembers.map(({ userKey, ID, nickname, exp }, index) => (
+            <tr key={userKey}>
               <td>
                 <Link className="nav-link" to={`/profile/${userKey}`}>
                   {index + 1}
