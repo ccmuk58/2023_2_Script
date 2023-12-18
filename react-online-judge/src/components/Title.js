fd7ef1427be231158c5a5b460cfe60644f89d7f4
Nakuri <ccmuk58@naver.com>
2023-12-18 19:07:45 +0900

    Feat: Temp Title

--- react-online-judge/src/components/Title.js
+++ react-online-judge/src/components/Title.js
@@ -2,63 +2,83 @@
 import { useEffect, useState } from "react";
 import { db } from "./firebaseinit";
 import { collection, getDocs } from "firebase/firestore";
+import { getProblemClassColor } from "../data/classColor";
 function Title() {
 	const [membersCount, setMembersCount] = useState(null);
 	const [problemsCount, setProblemsCount] = useState(null);
-  
-	useEffect(() => {
-	  const fetchMembersCount = async () => { // 회원수 가져오기
-		const membersCollection = collection(db, "Members");
-		const membersQuerySnapshot = await getDocs(membersCollection);
-		const count = membersQuerySnapshot.size;
-		setMembersCount(count);
-	  };
-  
-	  const fetchProblemsCount = async () => { // 문제수 가져오기
-		const problemsCollection = collection(db, "Problems");
-		const problemsQuerySnapshot = await getDocs(problemsCollection);
-		const count = problemsQuerySnapshot.size;
-		setProblemsCount(count);
-	  };
-  
-	  fetchMembersCount();
-	  fetchProblemsCount();
-	}, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 함
-  
-	
-  return (
-    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column bg-white">
-		<main class="px-3 pb-10">
-			<h1 class = "text-black">React-Online-Judge</h1>
-			<p class="lead text-black">This is a web application that provides a coding test environment.</p>
-			
-			<p class="lead">
-			<a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
-			</p>
-		</main>
 
-		<div class = "box-container">
-			<div class = "box">
-				<div class= "box-element">
-					<div class = "box-value"> {membersCount}명 </div>
-					<div class = "box-name"> 전체 회원 </div>	
+	useEffect(() => {
+		const fetchMembersCount = async () => { // 회원수 가져오기
+			const membersCollection = collection(db, "Members");
+			const membersQuerySnapshot = await getDocs(membersCollection);
+			const count = membersQuerySnapshot.size;
+			setMembersCount(count);
+		};
+
+		const fetchProblemsCount = async () => { // 문제수 가져오기
+			const problemsCollection = collection(db, "Problems");
+			const problemsQuerySnapshot = await getDocs(problemsCollection);
+			const count = problemsQuerySnapshot.size;
+			setProblemsCount(count);
+		};
+
+		fetchMembersCount();
+		fetchProblemsCount();
+	}, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 함
+
+
+	return (
+		<div className="content-item m-4">
+			<div className="mb-5">
+				<h1 className="header pb-0">ROJ</h1>
+				<h5 className="header pb-5">React-Online-Judge</h5>
+				<h7 className="pb-5">알고리즘 문제를 풀고, 순위를 경쟁하는 사이트입니다.</h7>
+			</div>
+
+			<div className="content-item-div p-5">
+				<div>
+					<h3>전체 문제</h3>
+					<br />
+					<h5>{problemsCount} 문제</h5>
 				</div>
-				<div class= "box-element">
-					<div class = "box-value"> {problemsCount}개 </div>
-					<div class = "box-name"> 전체 문제 </div>	
-				</div>
-				<div class = "box-element">
-					<div class = "box-value"> C, C++17, C#, Python3, Java, PHP </div>
-					<div class = "box-name"> 사용가능언어 </div>
+
+				<div>
+					<h3>사용자 수</h3>
+					<br />
+					<h5>asd</h5>
 				</div>
 			</div>
-		</div>
+			<div className="content-item-div p-5">
+				<div>
+					<h3>언어</h3>
+					<br />
+					<p className="classText">C</p>
+					<p className="classText">C++</p>
+					<p className="classText">C#</p>
+					<p className="classText">Java</p>
+					<p className="classText">Python</p>
+					<p className="classText">PHP</p>
+				</div>
+				<div>
+					<h3>클래스</h3>
+					<br />
+					<p className="classText"style={{color:getProblemClassColor("Ruby")}}>Ruby</p>
+					<p className="classText"style={{color:getProblemClassColor("Diamond")}}>Diamond</p>
+					<p className="classText"style={{color:getProblemClassColor("Platinum")}}>Platinum</p>
+					<p className="classText"style={{color:getProblemClassColor("Gold")}}>Gold</p>
+					<p className="classText"style={{color:getProblemClassColor("Silver")}}>Silver</p>
+					<p className="classText"style={{color:getProblemClassColor("Bronze")}}>Bronze</p>
 
-		<footer class="mt-auto text-white-50">
-			<p>made by {"\n"}20201554 최장현{"\n"}20201084 안형진</p>
-		</footer>
-	</div>
-  );
+				</div>
+			</div>
+
+			<div className="credit">
+				<p>made by {"\n"}20201554 최장현{"\n"}20201084 안형진</p>
+			</div>
+
+
+		</div>
+	);
 }
 
 export default Title;
