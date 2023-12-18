8d623121ae50601e2e225d0fbbba10606a898c62
RIHU <rihu0311@gmail.com>
2023-12-19 02:58:28 +0900

    logout 수정

--- react-online-judge/src/components/Logout.js
+++ react-online-judge/src/components/Logout.js
@@ -1,12 +1,17 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch} from "react-redux";
-import {LogoutState} from "./LoginReducer";
+import { useDispatch } from "react-redux";
+import { LogoutState } from "./LoginReducer";
 
-const Login = () => {
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+const Logout = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+    // useEffect 내에서 상태 업데이트와 페이지 이동을 보장
+  useEffect(() => {
     dispatch(LogoutState());
     navigate('/');
-}
+  }, [dispatch, navigate]);
 
-export default Login;
+};
+
+export default Logout;
--- react-online-judge/src/components/firebaseinit.js
+++ react-online-judge/src/components/firebaseinit.js
@@ -1,7 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics"; 
-import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -16,8 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
 const db = getFirestore(app);
 
 export {db}
